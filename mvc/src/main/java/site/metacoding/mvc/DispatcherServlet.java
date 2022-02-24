bed8707a68f1eaf74d90ded3947da81cfbe0002b
gih1214 <gih1214@hanmail.net>
2022-02-24 19:06:35 +0900

    MVC 패턴 적용 완료(DBCP,Request)

--- mvc/src/main/java/site/metacoding/mvc/DispatcherServlet.java
+++ mvc/src/main/java/site/metacoding/mvc/DispatcherServlet.java
@@ -1,25 +1,43 @@
 package site.metacoding.mvc;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import site.metacoding.mvc.config.DBConn;
+import site.metacoding.mvc.web.MyUserController;
 
-
+// http://localhost:8000/
+// http://localhost:8000/myuser
+// http://localhost:8000/mypost
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DBConn.getConnection();
-	}
+		String domain = request.getRequestURI();
+		System.out.println("domain : " + domain);
+		
+		if(domain.equals("/myuser")) {
+			MyUserController controller = new MyUserController(request, response);
+			
+			String idStr = request.getParameter("id"); // null
+			
+			if (idStr == null) {
+				controller.getAll();
+			} else {
+				int id = Integer.parseInt(idStr);
+				controller.getOne(id);
+			}
+		}
+	} // Dispatcher 스레드가 종료
+	// (1. 클라이언트가 연결된 response 선 짤림, 2. request 메모리영역 삭제, 3. DB connection 반납)
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-	}
+		
+	} // Dispatcher 스레드가 종료
 
 }
